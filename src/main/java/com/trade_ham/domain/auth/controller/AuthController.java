eef6826d94f8c35efd81fb6b4902aeb439a5eda0
alswp006 <alswp006@gmail.com>
2024-11-24 17:38:21 +0900

    [Update] 알림 읽음 로직 수정 (roki 코드리뷰 반영)

--- src/main/java/com/trade_ham/domain/auth/controller/AuthController.java
+++ src/main/java/com/trade_ham/domain/auth/controller/AuthController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/auth")
-public class AuthController {
+public class    AuthController {
 
     private final AuthService authService;
 
